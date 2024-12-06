b1469412e4a535b45f4f1dd9bd588e34b37fadca
Jeff Snow <thisisjeffsnow@gmail.com>
2024-12-06 07:21:45 -0500

    D6: Basic grab puzzle and print rows to start

---
+++ 2024/06 - C#/Parse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Basic code first. Can we grab all the rows?
+        string[] rows = File.ReadAllLines("input.txt");
+
+        // Can we print all the rows?
+        foreach (string row in rows)
+        {
+            Console.WriteLine(row);
+        }
+    }
+}
