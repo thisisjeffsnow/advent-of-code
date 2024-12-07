8a69e3759f8c188b87b9f42040c6432e31eed408
Jeff Snow <thisisjeffsnow@gmail.com>
2024-12-07 08:17:50 -0500

    D6: Part 2

--- 2024/06 - C#/Parse/Program.cs
+++ 2024/06 - C#/Parse/Program.cs
@@ -1,9 +1,52 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 class Program
 {
+
+    static bool SimulatePath(int startX, int startY, int startDir, char[][] map, int[][] dirs, int dirMod, int mapH, int mapW)
+    {
+        // Track the squares the guard visits with their direction at each square.
+        // If we ever see the guard in the same spot with the same direction again -> it's a loop.
+        HashSet<(int x, int y, int dir)> visited = new HashSet<(int x, int y, int dir)>();
+        int currX = startX, currY = startY, currDir = startDir;
+
+        Boolean done = false;
+
+        while (!done)
+        {
+            // Have we been here before with this direction? If so, we're looping.
+            if (visited.Contains((currX, currY, currDir))) {
+                return true; // LOOP!
+            }
+
+            visited.Add((currX, currY, currDir));
+
+            // Figure out where we're going next.
+            int nextY = currY + dirs[currDir][0];
+            int nextX = currX + dirs[currDir][1];
+
+            // Check if we've wandered off the edge of the map. If so, no loop!
+            if (nextY < 0 || nextY >= mapH || nextX < 0 || nextX >= mapW) {
+                done = true;
+                break;
+            }
+
+            // Check if we hit an obstacle. If so, pivot.
+            if (map[nextY][nextX] == '#')  {
+                currDir = (currDir + 1) % dirMod; // Turn right
+            }
+            else {
+                currX = nextX;
+                currY = nextY;
+            } // Move forward
+        }
+
+        // If we break out of the loop (harhar), then it's not really a loop. The guard exited some other way.
+        return false;
+    }
+
     static void Main(string[] args)
     {
         // Grab file rows
@@ -13,49 +56,34 @@ class Program
         int mapW = rows[0].Length;
 
         // Move rows to a writable map, find all obstacles and find start
+        // We're gonna need to track obstacles and the guard's starting position.
+        // Going to map obstacles by row and column too for Part 2?
+        // Dictionary<int, List<(int x, int y)>> obstaclesByRow = new Dictionary<int, List<(int x, int y)>>();
+        // Dictionary<int, List<(int x, int y)>> obstaclesByCol = new Dictionary<int, List<(int x, int y)>>();
+
         List<(int x, int y)> obstacles = new List<(int x, int y)>();
-
-        // Going to map obstacles by row and column too for Part 2.
-        Dictionary<int, List<(int x, int y)>> obstaclesByRow = new Dictionary<int, List<(int x, int y)>>();
-        Dictionary<int, List<(int x, int y)>> obstaclesByCol = new Dictionary<int, List<(int x, int y)>>();
-
-        int currX = -1;
-        int currY = -1;
-
         char[][] map = new char[mapH][];
+        int startX = -1, startY = -1; // Start uninitialized for now.
+
+        // Fill the map array and find the guard's starting position (^).
         for (int i = 0; i < mapH; i++)
-        {
             map[i] = new char[mapW];
-        }
 
         for (int y = 0; y < mapH; ++y)
         {
             for (int x = 0; x < mapW; ++x)
             {
-                if (rows[y][x] == '^') // Find start
-                {
-                    currX = x;
-                    currY = y;
-                }
-                else if (rows[y][x] == '#') // Find obstacles
-                {
-                    obstacles.Add((x, y));
-                    if (!obstaclesByRow.ContainsKey(y))
-                    {
-                        obstaclesByRow[y] = new List<(int x, int y)>();
-                    }
-                    obstaclesByRow[y].Add((x, y));
-
-                    if (!obstaclesByCol.ContainsKey(x))
-                    {
-                        obstaclesByCol[x] = new List<(int x, int y)>();
-                    }
-                    obstaclesByCol[x].Add((x, y));
-                }
                 map[y][x] = rows[y][x];
+                if (rows[y][x] == '^') {
+                    // Found guard, keep x and y.
+                    startX = x;
+                    startY = y;
+                }
+                if (rows[y][x] == '#') {
+                     obstacles.Add((x, y)); // obstacles
+                }
             }
         }
-
 
         int[][] dirs = new int[4][];
         dirs[0] = new int[] { -1, 0 }; // north Y - 1, X
@@ -65,18 +93,23 @@ class Program
         int dirMod = dirs.Length;
         int currDirIndex = 0;
 
+        // P1: Count visited spots by guard in original path.
+        int visited = 0;
+        HashSet<(int x, int y)> visitedSpots = new HashSet<(int x, int y)>();
+        int currX = startX;
+        int currY = startY;
+
         Boolean done = false;
         // So, we move north (y - 1, x) until we hit # (or out of bounds = done), then rotate and go east (y, x + 1)
         // until we hit # (or out of bounds = done), then south, (y + 1, x), then west, (y, x - 1), over and over.
         // Until we go out of bounds. We'll keep track of each new square we visit by marking the cell with an X for visited.
 
-        int visited = 0;
         while (!done)
         {
-            if (map[currY][currX] != 'X')
+            if (!visitedSpots.Contains((currX, currY)))
             {
-                ++visited;
-                map[currY][currX] = 'X';
+                visited++;
+                visitedSpots.Add((currX, currY));
             }
 
             // What is our next move?
@@ -84,8 +117,7 @@ class Program
             int nextX = currX + dirs[currDirIndex][1];
 
             // If we escape the map, we're done. 
-            if (nextY < 0 || nextY >= mapH || nextX < 0 || nextX >= mapW)
-            {
+            if (nextY < 0 || nextY >= mapH || nextX < 0 || nextX >= mapW) {
                 done = true;
                 break;
             }
@@ -97,7 +129,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("Currently at: " + currX + ", " + currY + "\n");
+                Console.WriteLine("Currently at: " + currX + ", " + currY);
                 currX = nextX;
                 currY = nextY;
             }
@@ -105,19 +137,35 @@ class Program
 
         Console.WriteLine("Spots visited: " + visited);
 
-        // Check we can print obstacles by row and col:
-        Console.WriteLine("Rows:");
-        foreach (var row in obstaclesByRow)
+        // P2: Block off all visited spots one by one on original path and check for looping.
+        int loopSpots = 0;
+
+        foreach (var (x, y) in visitedSpots)
         {
-            Console.WriteLine($"Row {row.Key}: {string.Join(", ", row.Value)}");
+            // We can't put an obstacle at the starting position.
+            if (x == startX && y == startY) {
+                continue;
+            }
+
+            // Make a fresh copy of the map to test this position.
+            char[][] mapCopy = new char[mapH][];
+            for (int i = 0; i < mapH; i++) {
+                mapCopy[i] = (char[])map[i].Clone();
+            }
+
+            // Place the new obstacle.
+            mapCopy[y][x] = '#';
+
+            // Console.WriteLine($"Placing block at: ({x}, {y})");
+
+            // Simulate the guard's patrol with the new obstacle.
+            if (SimulatePath(startX, startY, 0, mapCopy, dirs, dirMod, mapH, mapW))
+            {
+                // Console.WriteLine($"Loop detected with: ({x}, {y})");
+                loopSpots++;
+            }
         }
 
-        // Print encountered obstacles by column
-        Console.WriteLine("Cols:");
-        foreach (var col in obstaclesByCol)
-        {
-            Console.WriteLine($"Column {col.Key}: {string.Join(", ", col.Value)}");
-        }
+        Console.WriteLine("Loop spots: " + loopSpots);
     }
-
 }
