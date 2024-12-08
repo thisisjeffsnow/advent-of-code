60e8e88aa14d7ed87e6e1a552ca4084d2d1d0bd6
Jeff Snow <thisisjeffsnow@gmail.com>
2024-12-08 10:42:15 -0500

    D7: Part 2

--- 2024/07 - C/parse.c
+++ 2024/07 - C/parse.c
@@ -9,8 +9,23 @@
 // might be a security problem if it was used in a real project. I'm not sure.
 // It would be worth investigating though.
 
+// Problem 2 concatenate operator
+uint64_t concatenate(uint64_t left, uint64_t right)
+{
+    // Again, I'm using LINE_SIZE here because I know it's of sufficient size for this problem.
+    // But, I'm sure there are better ways to do this.
+    char leftBuffer[LINE_SIZE];
+    char rightBuffer[LINE_SIZE];
+    snprintf(leftBuffer, LINE_SIZE, "%" PRIu64, left);
+    snprintf(rightBuffer, LINE_SIZE, "%" PRIu64, right);
+
+    strcat(leftBuffer, rightBuffer);
+
+    return strtoull(leftBuffer, NULL, 10);
+}
+
 // DFS Helper for checking all adding and multiplying combinations once we grab all our numbers.
-int dfs(uint64_t *rightNumbers, size_t rightNumbersSize, size_t currIndex, uint64_t currentCalculation, uint64_t leftNumber)
+int dfs(uint64_t *rightNumbers, size_t rightNumbersSize, size_t currIndex, uint64_t currentCalculation, uint64_t leftNumber, int problemNumber)
 {
     // Base Case = We have exhausted all the numbers on the right side, now check that the calcuation is good or not.
     if (currIndex == rightNumbersSize)
@@ -22,15 +37,25 @@ int dfs(uint64_t *rightNumbers, size_t rightNumbersSize, size_t currIndex, uint6
     uint64_t nextRightNumber = rightNumbers[currIndex];
 
     // Add
-    if (dfs(rightNumbers, rightNumbersSize, currIndex + 1, currentCalculation + nextRightNumber, leftNumber))
+    if (dfs(rightNumbers, rightNumbersSize, currIndex + 1, currentCalculation + nextRightNumber, leftNumber, problemNumber))
     {
         return 1;
     }
 
     // Multiply
-    if (dfs(rightNumbers, rightNumbersSize, currIndex + 1, currentCalculation * nextRightNumber, leftNumber))
+    if (dfs(rightNumbers, rightNumbersSize, currIndex + 1, currentCalculation * nextRightNumber, leftNumber, problemNumber))
     {
         return 1;
+    }
+
+    // Concatenate (Only for part 2)
+    if (problemNumber == 2)
+    {
+        uint64_t concatenated = concatenate(currentCalculation, nextRightNumber);
+        if (dfs(rightNumbers, rightNumbersSize, currIndex + 1, concatenated, leftNumber, problemNumber))
+        {
+            return 1;
+        }
     }
 
     return 0;
@@ -55,6 +80,7 @@ int main()
 
     size_t bytesRead = 0;
     uint64_t p1Solution = 0;
+    uint64_t p2Solution = 0;
 
     while (fgets(lp, LINE_SIZE, fp) != NULL)
     {
@@ -123,16 +149,22 @@ int main()
         // If there are no numbers on the right, do nothing.
         if (currIndex > 0)
         {
-            if (dfs(rightNumbers, currIndex, 1, rightNumbers[0], leftNumber))
+            if (dfs(rightNumbers, currIndex, 1, rightNumbers[0], leftNumber, 1))
             {
-                printf("Solution found! %" PRIu64 "\n", leftNumber);
+                printf("Solution1 found! %" PRIu64 "\n", leftNumber);
                 p1Solution += leftNumber;
+            }
+            if (dfs(rightNumbers, currIndex, 1, rightNumbers[0], leftNumber, 2))
+            {
+                printf("Solution2 found! %" PRIu64 "\n", leftNumber);
+                p2Solution += leftNumber;
             }
         }
     }
 
     printf("Bytes Read = %zu\n", bytesRead);
     printf("P1 Solution = %" PRIu64 "\n", p1Solution);
+    printf("P2 Solution = %" PRIu64 "\n", p2Solution);
 
     free(lp);
     fclose(fp);
