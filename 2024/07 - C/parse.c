e15b2a34293d21bb52dca922dbe875fc96e6f347
Jeff Snow <thisisjeffsnow@gmail.com>
2024-12-08 09:08:28 -0500

    D7: Grab the puzzle line by line in C

--- 2024/07 - C/parse.c
+++ 2024/07 - C/parse.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FILE_NAME "input.txt"
-#define CHUNK_SIZE 1024
+#define LINE_SIZE 1024 // Examining puzzle shows that 1024 is more than sufficient for a line length.
 
 int main() {
     FILE *fp = fopen(FILE_NAME, "r");
@@ -11,52 +12,25 @@ int main() {
         return 1;
     }
 
-    char *puzzleInput = NULL;
-    size_t size = 0;
-    size_t read = 0;
-
-    int done = 0;
-
-    while (!done) {
-        char *newChunk = realloc(puzzleInput, size + CHUNK_SIZE);
-        if (newChunk == NULL) {
-            perror("Failed to grab chunk from file.");
-            free(puzzleInput);
-            fclose(fp);
-            return 1;
-        }
-
-        puzzleInput = newChunk;
-        size += CHUNK_SIZE;
-
-        size_t bytesRead = fread(puzzleInput + read, 1, CHUNK_SIZE, fp);
-        printf("Bytes read on this pass... %zu (total so far: %zu)\n", bytesRead, read);
-        read += bytesRead;
-
-        if (bytesRead < CHUNK_SIZE) {
-            if (feof(fp)) {
-                done = 1; // end of file
-                break;
-            } else if (ferror(fp)) {
-                perror("Error reading input file.");
-                free(puzzleInput);
-                fclose(fp);
-                return 1;
-            }
-        }
+    char *linePointer = malloc(LINE_SIZE);
+    if (linePointer == NULL) {
+        perror("Couldn't create a line pointer.");
+        fclose(fp);
+        return 1;
     }
+
+    size_t bytesRead = 0;
+
+    while (fgets(linePointer, LINE_SIZE, fp) != NULL) {
+        size_t lineLength = strlen(linePointer);
+        bytesRead += lineLength;
+        printf("Got a line = %s", linePointer);
+        printf("Line length = %zu\n************************\n", lineLength);
+    }
+
+    printf("Bytes Read = %zu\n", bytesRead);
+
+    free(linePointer);
     fclose(fp);
-
-    if (read == 0) {
-        printf("File is empty!\n");
-        free(puzzleInput);
-        return 0;
-    }
-
-    puzzleInput[read] = '\0';
-
-    printf("Total bytes read: %zu\n", read);
-
-    free(puzzleInput);
     return 0;
 }
