3252204cb81a227895566c2669cf6569dc257bd5
Jeff Snow <thisisjeffsnow@gmail.com>
2024-12-08 09:24:00 -0500

    D7: Grab the left hand number from each line

--- 2024/07 - C/parse.c
+++ 2024/07 - C/parse.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #define FILE_NAME "input.txt"
-#define LINE_SIZE 1024 // Examining puzzle shows that 1024 is more than sufficient for a line length.
+#define LINE_SIZE 1024 // Puzzle lines don't exceed this size.
+// Probably better to dyanmically figure out the longest line. This
+// might be a security problem if it was used in a real project. I'm not sure.
+// It would be worth investigating though.
 
 int main() {
     FILE *fp = fopen(FILE_NAME, "r");
@@ -12,8 +16,8 @@ int main() {
         return 1;
     }
 
-    char *linePointer = malloc(LINE_SIZE);
-    if (linePointer == NULL) {
+    char *lp = malloc(LINE_SIZE);
+    if (lp == NULL) {
         perror("Couldn't create a line pointer.");
         fclose(fp);
         return 1;
@@ -21,16 +25,40 @@ int main() {
 
     size_t bytesRead = 0;
 
-    while (fgets(linePointer, LINE_SIZE, fp) != NULL) {
-        size_t lineLength = strlen(linePointer);
+    while (fgets(lp, LINE_SIZE, fp) != NULL) {
+        size_t lineLength = strlen(lp);
         bytesRead += lineLength;
-        printf("Got a line = %s", linePointer);
-        printf("Line length = %zu\n************************\n", lineLength);
+        printf("Got a line = %s", lp);
+        printf("Line length = %zu\n", lineLength);
+
+        // Alright, I figured out how to grab lines. Can we separate the numbers?
+        // AAAA: B C D E F G .....
+
+        // Find the colon.
+        char *colonIndex = strchr(lp, ':');
+        if(colonIndex == NULL) {
+            perror("Found a line without a colon. Exiting.");
+            fclose(fp);
+            return 1;
+        }
+
+        // Grab the left number. Change the colon to string terminator.
+        *colonIndex = '\0';
+        // Puzzle shows that the left number is always positive, so use unsigned and a big storage for number.
+        uint64_t leftNumber = strtoull(lp, NULL, 10); 
+        // Test
+        printf("Left Number = %" PRIu64 "\n", leftNumber);
+        printf("**********\n\n");
+
+
+
+
+
     }
 
     printf("Bytes Read = %zu\n", bytesRead);
 
-    free(linePointer);
+    free(lp);
     fclose(fp);
     return 0;
 }
